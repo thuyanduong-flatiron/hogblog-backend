ce029a8630f126b481b1d8e86d71f1b386195a6f
thuyanduong-flatiron <thuyan.duong@flatironschool.com>
2019-01-16 11:33:37 -0500

    changed sqlite to pg and updated ruby

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_01_16_053410) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "hogs", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
